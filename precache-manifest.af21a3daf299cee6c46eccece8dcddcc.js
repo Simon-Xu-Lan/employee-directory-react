4bb583cc159300b58769e2c5c2fbc799bd13dd21
Simon Xu <xulan20907@gmail.com>
2020-08-20 12:13:11 -0600

    Updates

---
+++ precache-manifest.af21a3daf299cee6c46eccece8dcddcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f601695ba1e7f8114645d5928083a033",
+    "url": "/employee-directory-react/index.html"
+  },
+  {
+    "revision": "389372889611bcc3c3f9",
+    "url": "/employee-directory-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d247ba35e8cfb5798b7a",
+    "url": "/employee-directory-react/static/js/2.8a159b76.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/employee-directory-react/static/js/2.8a159b76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "389372889611bcc3c3f9",
+    "url": "/employee-directory-react/static/js/main.a70fdfd5.chunk.js"
+  },
+  {
+    "revision": "7c483b2024677cc109e7",
+    "url": "/employee-directory-react/static/js/runtime-main.0757c0fd.js"
+  }
+]);
